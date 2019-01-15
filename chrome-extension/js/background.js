f012026d7f5de67b963f08f2a2e1d1a3fd7ea440
hogesuke <miyado@gmail.com>
2019-01-15 23:09:24 +0900

    Twitterが開かれた際にステータスが更新されるように修正

--- chrome-extension/js/background.js
+++ chrome-extension/js/background.js
@@ -2,6 +2,7 @@ chrome.tabs.onUpdated.addListener(tabId => chrome.pageAction.show(tabId));
 
 // todo tokenが取得できなかった場合の処理
 const token = localStorage.getItem('token');
+let isChikurying = false;
 
 const postProfile = profile => {
   return fetch('https://slack.com/api/users.profile.set', {
@@ -14,16 +15,28 @@ const postProfile = profile => {
   });
 };
 
-chrome.tabs.onUpdated.addListener(tab => {
-  if (/^https:\/\/twitter\.com/.test(tab.url)) {
-    postProfile({ status_text: 'aaa', status_emoji: ':herb:' });
+const clearProfile = () => {
+  // Twitterのタブがひとつも開いていなければステータスを更新
+  chrome.tabs.query({ url: 'https://twitter.com/*' }, tabs => {
+    if (tabs.length <= 0) {
+      postProfile({ status_text: 'bbb', status_emoji: ':palm_tree:' })
+        .then(() => isChikurying = false);
+    }
+  });
+};
+
+chrome.tabs.onUpdated.addListener((tabId, changeInfo) => {
+  if (!changeInfo.url) { return; }
+
+  // Twitterが開かれたら
+  if (/^https:\/\/twitter\.com/.test(changeInfo.url) && !isChikurying) {
+    return postProfile({ status_text: 'aaa', status_emoji: ':herb:' })
+      .then(() => isChikurying = true);
   }
+
+  clearProfile();
 });
 
 chrome.tabs.onRemoved.addListener(() => {
-  chrome.tabs.query({ url: 'https://twitter.com/*' }, tabs => {
-    if (tabs.length <= 0) {
-      postProfile({ status_text: 'bbb', status_emoji: ':palm_tree:' });
-    }
-  });
+  clearProfile();
 })
