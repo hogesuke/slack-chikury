6a86ac93acecb0daefd4a410d391fbd326623e9e
hogesuke <miyado@gmail.com>
2019-01-15 23:34:28 +0900

    リファクタリング

--- chrome-extension/js/background.js
+++ chrome-extension/js/background.js
@@ -4,6 +4,14 @@ chrome.tabs.onUpdated.addListener(tabId => chrome.pageAction.show(tabId));
 const token = localStorage.getItem('token');
 let isChikurying = false;
 
+const validateTwitterTabExistence = async () => {
+  return new Promise(resolve => {
+    chrome.tabs.query({ url: 'https://twitter.com/*' }, tabs => {
+      resolve(tabs.length > 0);
+    });
+  });
+};
+
 const postProfile = profile => {
   return fetch('https://slack.com/api/users.profile.set', {
     method: 'POST',
@@ -15,28 +23,33 @@ const postProfile = profile => {
   });
 };
 
-const clearProfile = () => {
-  // Twitterのタブがひとつも開いていなければステータスを更新
-  chrome.tabs.query({ url: 'https://twitter.com/*' }, tabs => {
-    if (tabs.length <= 0) {
-      postProfile({ status_text: 'bbb', status_emoji: ':palm_tree:' })
-        .then(() => isChikurying = false);
-    }
-  });
+const chikuru = () => {
+  postProfile({ status_text: 'aaa', status_emoji: ':herb:' }).then(
+    () => (isChikurying = true)
+  );
+};
+
+const clear = () => {
+  postProfile({ status_text: 'bbb', status_emoji: ':palm_tree:' }).then(
+    () => (isChikurying = false)
+  );
 };
 
 chrome.tabs.onUpdated.addListener((tabId, changeInfo) => {
-  if (!changeInfo.url) { return; }
-
-  // Twitterが開かれたら
-  if (/^https:\/\/twitter\.com/.test(changeInfo.url) && !isChikurying) {
-    return postProfile({ status_text: 'aaa', status_emoji: ':herb:' })
-      .then(() => isChikurying = true);
+  if (!changeInfo.url) {
+    return;
   }
 
-  clearProfile();
+  // Twitterが開かれたら
+  if (/^https:\/\/twitter\.com/.test(changeInfo.url)) {
+    !isChikurying && chikuru();
+  } else {
+    isChikurying &&
+      validateTwitterTabExistence().then(exists => !exists && clear());
+  }
 });
 
 chrome.tabs.onRemoved.addListener(() => {
-  clearProfile();
-})
+  isChikurying &&
+    validateTwitterTabExistence().then(exists => !exists && clear());
+});
