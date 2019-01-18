ed1b5f6e49ca95c9951aa9700c08191aef2812d4
hogesuke <miyado@gmail.com>
2019-01-18 23:49:33 +0900

    tokenの入力がない場合はsaveボタンをdisabledにするように修正

--- chrome-extension/js/popup.js
+++ chrome-extension/js/popup.js
@@ -1,8 +1,21 @@
 document.addEventListener('DOMContentLoaded', () => {
-  const token = document.querySelector('#input-token');
+  const tokenInput = document.querySelector('#input-token');
   const saveButton = document.querySelector('#save-button');
+  const token = localStorage.getItem('token') || '';
+
+  tokenInput.value = token;
+
+  if (!tokenInput.value) {
+    saveButton.disabled = true;
+  }
+
+  tokenInput.addEventListener('input', () => {
+    saveButton.disabled = !tokenInput.value;
+  });
 
   saveButton.addEventListener('click', () => {
-    localStorage.setItem('token', token.value);
+    if (tokenInput.value) {
+      localStorage.setItem('token', tokenInput.value);
+    }
   });
 });
