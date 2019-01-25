4ff9cc897adf11f27107e8fde8cb03141d5adace
hogesuke <miyado@gmail.com>
2019-01-25 17:07:32 +0900

    saveボタンを削除

--- src/js/popup.js
+++ src/js/popup.js
@@ -2,10 +2,8 @@ import '../css/style.scss';
 
 document.addEventListener('DOMContentLoaded', () => {
   const tokenInput = document.querySelector('.input-token');
-  const saveButton = document.querySelector('.save-button');
-  const token = localStorage.getItem('token') || '';
 
-  tokenInput.value = token;
+  tokenInput.value = localStorage.getItem('token') || '';
 
   const startTimeInput = document.querySelector('.input-starttime');
   const endTimeInput = document.querySelector('.input-endtime');
@@ -15,18 +13,8 @@ document.addEventListener('DOMContentLoaded', () => {
   startTimeInput.value = startTime;
   endTimeInput.value = endTime;
 
-  if (!tokenInput.value) {
-    saveButton.disabled = true;
-  }
-
   tokenInput.addEventListener('input', () => {
-    saveButton.disabled = !tokenInput.value;
-  });
-
-  saveButton.addEventListener('click', () => {
-    if (tokenInput.value) {
-      localStorage.setItem('token', tokenInput.value);
-    }
+    localStorage.setItem('token', tokenInput.value);
   });
 
   startTimeInput.addEventListener('input', () => {
