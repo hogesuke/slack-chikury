93242914cb90840f584d8bd5733842fcab63b59e
hogesuke <miyado@gmail.com>
2019-02-26 23:45:12 +0900

    日付をまたいだ際に経過時間がundefinedになる不具合の修正

--- src/js/time-calculator.js
+++ src/js/time-calculator.js
@@ -2,6 +2,7 @@ import WebStorage from './web-storage';
 
 export default class TimeCalculator {
 
+  // TODO: 引数のfromDateいる？
   static calcTotalSaboriTime(fromDate) {
     const lastUpdateMinutes = parseInt(WebStorage.getProgressedMinutes()) || 0;
     const savedSeconds = parseInt(WebStorage.getProgressedSeconds()) || 0;
@@ -19,7 +20,10 @@ export default class TimeCalculator {
 
     // 日付をまたいだ場合リセット
     if (this._getYYYYMD(lastDate) !== this._getYYYYMD(currentDate)) {
-      return 0;
+      return {
+        seconds: 0,
+        minutes: 0
+      };
     }
 
     const seconds = ((currentDate - fromDate) / 1000) + savedSeconds
